c9b882ce2043f13c2a1d4c616ff77326941c910e
sherwyn <sherwyn.sen@gmail.com>
2019-10-25 18:32:52 -0700

    fix indenting

--- frontend_exported/js/authentication.js
+++ frontend_exported/js/authentication.js
@@ -3,123 +3,127 @@ var AuthenticationDetails = AmazonCognitoIdentity.AuthenticationDetails;
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
 var GLOBALS = {
-    poolData: {
-        UserPoolId: "us-west-2_XMbIozUhu",
-        ClientId: "690fin3rp1u9hrhhbi360uvpnl"
-    },
+  poolData: {
+    UserPoolId: "us-west-2_XMbIozUhu",
+    ClientId: "690fin3rp1u9hrhhbi360uvpnl"
+  },
 };
 
 function authAjax(url, ajaxData, processUsernameCallback, afterResponseCallback) {
-    let userPool = new CognitoUserPool(GLOBALS.poolData);
-    let cognitoUser = userPool.getCurrentUser();
+  let userPool = new CognitoUserPool(GLOBALS.poolData);
+  let cognitoUser = userPool.getCurrentUser();
 
-    if (cognitoUser == null) {
-        afterResponseCallback(false);
-        return;
+  if (cognitoUser == null) {
+    afterResponseCallback(false);
+    return;
+  }
+
+  cognitoUser.getSession((error, session) => {
+    if (error) {
+      console.log("cognito session error: " + error);
+      return;
     }
+    if (session) {
+      processUsernameCallback(cognitoUser.username);
+    }
+  });
 
-    cognitoUser.getSession((error, session) => {
-        if (error) {
-            console.log("cognito session error: " + error);
-            return;
-        }
-        if (session) {
-            processUsernameCallback(cognitoUser.username);
-        }
+  ajaxData.accessToken = cognitoUser.signInUserSession.accessToken.jwtToken;
+
+  $.post(url, ajaxData)
+    .done((responseData) => {
+      afterResponseCallback(responseData);
     });
-
-    ajaxData.accessToken = cognitoUser.signInUserSession.accessToken.jwtToken;
-
-    $.post(url, ajaxData)
-        .done((responseData) => {
-            afterResponseCallback(responseData);
-        });
 }
 
 
 function authAjaxWithFile(url, file, otherFields, processUsernameCallback, afterResponseCallback) {
-    let fd = new FormData();
-    fd.append(fileField, file);
-    let data = [];
-    data.push(otherFields);
-    fd.append("data", JSON.stringify(data));
-    authAjax(url, data, processUsernameCallback, afterResponseCallback);
+  let fd = new FormData();
+  fd.append(fileField, file);
+  let data = [];
+  data.push(otherFields);
+  fd.append("data", JSON.stringify(data));
+  authAjax(url, data, processUsernameCallback, afterResponseCallback);
 }
 
-function wrapCallback(callback){
-    return {
-        onFailure: (err)=>{callback(err, null);},
-        onSuccess: (result)=>{callback(null, result);}
-    };
+function wrapCallback(callback) {
+  return {
+    onFailure: (err) => {
+      callback(err, null);
+    },
+    onSuccess: (result) => {
+      callback(null, result);
+    }
+  };
 }
 
 
 function isLoggedIn() {
-    let userPool = new CognitoUserPool(GLOBALS.poolData);
-    let cognitoUser = userPool.getCurrentUser();
-    if (cognitoUser == null)
-        return false;
-    return cognitoUser.Session != null;
+  let userPool = new CognitoUserPool(GLOBALS.poolData);
+  let cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser == null)
+    return false;
+  return cognitoUser.Session != null;
 }
 
 
-function getUser(userName){
-    let userPool = new CognitoUserPool(GLOBALS.poolData);
-    let cognitoUser = userPool.getCurrentUser();
-    if (cognitoUser===undefined){
-        let userData = {
-            Username : userName,
-            Pool : userPool
-        };
-        cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    }
-    return cognitoUser;
+function getUser(userName) {
+  let userPool = new CognitoUserPool(GLOBALS.poolData);
+  let cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser === undefined) {
+    let userData = {
+      Username: userName,
+      Pool: userPool
+    };
+    cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  }
+  return cognitoUser;
 }
 
 
 function signUpUser(userName, userEmail, userPassword, callback) {
-    let dataEmail = {
-        Name : 'email',
-        Value : userEmail
-    };
-    let dataName = {
-        Name : 'preferred_username',
-        Value : userName
-    };
-    let attributeList = [ new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail),
-        new AmazonCognitoIdentity.CognitoUserAttribute(dataName) ];
+  let dataEmail = {
+    Name: 'email',
+    Value: userEmail
+  };
+  let dataName = {
+    Name: 'preferred_username',
+    Value: userName
+  };
+  let attributeList = [new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail),
+    new AmazonCognitoIdentity.CognitoUserAttribute(dataName)];
 
-    let userPool = new CognitoUserPool(GLOBALS.poolData);
-    userPool.signUp(userName, userPassword, attributeList, null, function(err, result){
-        if (err) {
-            callback(err, null);
-        }
-        else {
-            callback(null, result);
-        }
-    });
-}
-
-function signInUser(username, password, callback){
-    let authenticationData = {
-        Username : username, Password : password,
-    };
-    let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-    getUser(username).authenticateUser(authenticationDetails, wrapCallback(callback));
-}
-
-
-function signOutUser(){
-    let userPool = new CognitoUserPool(GLOBALS.poolData);
-    let cognitoUser = userPool.getCurrentUser();
-    if (cognitoUser != null){
-        if (cognitoUser.signInUserSession){
-            console.log("signing out...");
-            cognitoUser.globalSignOut();
-        }
+  let userPool = new CognitoUserPool(GLOBALS.poolData);
+  userPool.signUp(userName, userPassword, attributeList, null, function (err, result) {
+    if (err) {
+      callback(err, null);
     }
+    else {
+      callback(null, result);
+    }
+  });
+}
 
-    // clear cache ourselves because amazon didnt do it...
-    this.storage.clear();
+function signInUser(username, password, callback) {
+  let authenticationData = {
+    Username: username, Password: password,
+  };
+  let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  getUser(username).authenticateUser(authenticationDetails, wrapCallback(callback));
+}
+
+
+function signOutUser() {
+  let userPool = new CognitoUserPool(GLOBALS.poolData);
+  let cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser != null) {
+    if (cognitoUser.signInUserSession) {
+      console.log("signing out...");
+      cognitoUser.globalSignOut();
+    }
+  }
+
+  // clear cache ourselves because amazon didnt do it...
+  this.storage.clear();
 }
 
--- frontend_exported/js/routeFunctions.js
+++ frontend_exported/js/routeFunctions.js
@@ -1,51 +1,52 @@
-    let EC2_URL = "http://ec2-34-221-65-162.us-west-2.compute.amazonaws.com:5000";
-    let LOCAL_URL = "http://0.0.0.0:5000";
-    let EC2_bucket_URL = "http://s3.fumblr.club";
-    let SERVER_URL = LOCAL_URL;
-    let IMAGE_HOST_URL = "http://d35f612x9d99xv.cloudfront.net/"; // this needs a slash at the end
+let EC2_URL = "http://ec2-34-221-65-162.us-west-2.compute.amazonaws.com:5000";
+let LOCAL_URL = "http://0.0.0.0:5000";
+let EC2_bucket_URL = "http://s3.fumblr.club";
+let SERVER_URL = LOCAL_URL;
+let IMAGE_HOST_URL = "http://d35f612x9d99xv.cloudfront.net/"; // this needs a slash at the end
 
-    function getCurrentUser(){
-        console.log($.post(SERVER_URL + "/get_current_user"));
-    }
+function getCurrentUser() {
+  console.log($.post(SERVER_URL + "/get_current_user"));
+}
 
-    function createUserWithProfilePicture(username, birthday, firstName, lastName, bio, profilePicture) {
-        postWithFile(
-            SERVER_URL + "/create_user",
-            profilePicture,
-            "profilePicture",
-            {
-                username: username,
-                birthday: birthday,
-                firstName: firstName,
-                lastName: lastName,
-                bio: bio,
-            });
-    }
-        function createUser(username, birthday, firstName, lastName, bio) {
-    	console.log(
-	    	$.post(SERVER_URL + "/create_user",
-	    	{
-		        username: username,
-		        birthday: birthday,
-		        firstName: firstName,
-		        lastName: lastName,
-		        bio: bio,
-     		})
-     	);
-    }
+function createUserWithProfilePicture(username, birthday, firstName, lastName, bio, profilePicture) {
+  postWithFile(
+    SERVER_URL + "/create_user",
+    profilePicture,
+    "profilePicture",
+    {
+      username: username,
+      birthday: birthday,
+      firstName: firstName,
+      lastName: lastName,
+      bio: bio,
+    });
+}
+
+function createUser(username, birthday, firstName, lastName, bio) {
+  console.log(
+    $.post(SERVER_URL + "/create_user",
+      {
+        username: username,
+        birthday: birthday,
+        firstName: firstName,
+        lastName: lastName,
+        bio: bio,
+      })
+  );
+}
 
 //for signup.html
-    $( document ).ready(function(e) {
-        $(".createUserButton").click(function(){
-            createUserTest(document.getElementById('createUser').value, document.getElementById('createUserBirthday').value, document.getElementById('createUserFirstName').value, document.getElementById('createUserLastName').value)
-        });
-    });
+$(document).ready(function (e) {
+  $(".createUserButton").click(function () {
+    createUserTest(document.getElementById('createUser').value, document.getElementById('createUserBirthday').value, document.getElementById('createUserFirstName').value, document.getElementById('createUserLastName').value)
+  });
+});
 
-    function viewUserProfile(user) {
+function viewUserProfile(user) {
 
-    		$.post(SERVER_URL + "/user/" + user, function(user_data){
-                console.log(user_data);
-                let profile_html_to_append = '';
+  $.post(SERVER_URL + "/user/" + user, function (user_data) {
+    console.log(user_data);
+    let profile_html_to_append = '';
 
 profile_html_to_append +=   '<div class="col-xs-12 column-1 col-xl-6 offset-xl-3">' +
                               '<div class="subgrid">' +
@@ -89,210 +90,206 @@ profile_html_to_append +=   '<div class="col-xs-12 column-1 col-xl-6 offset-xl-3
 
     }
 
-    function editProfile(user, firstName, lastName, bio, profilePicture){
+function editProfile(user, firstName, lastName, bio, profilePicture) {
 
-    	console.log(
-	    	$.post(SERVER_URL + "/edit_profile",
-	    	{
-		        currentUser: user,
-		        firstName: firstName,
-		        lastName: lastName,
-		        bio: bio
-		        //profilePicture
-     		})
-     	);
-    }
+  console.log(
+    $.post(SERVER_URL + "/edit_profile",
+      {
+        currentUser: user,
+        firstName: firstName,
+        lastName: lastName,
+        bio: bio
+        //profilePicture
+      })
+  );
+}
 
 //for profilepage.html
-    $( document ).ready(function(e) {
-        $(".editProfileButton").click(function(){
-            editProfile(document.getElementById('editFirstName').value, document.getElementById('editLastName').value, document.getElementById('editBio').value)
-        });
-    });
+$(document).ready(function (e) {
+  $(".editProfileButton").click(function () {
+    editProfile(document.getElementById('editFirstName').value, document.getElementById('editLastName').value, document.getElementById('editBio').value)
+  });
+});
 
 //for uploadpost.html
-    function createPost(user, text) {
-        console.log(
-            $.post(SERVER_URL + "/create_post",
-                {
-                    currentUser: user,
-                    text: text,
-                })
-        );
-    }
+function createPost(user, text) {
+  console.log(
+    $.post(SERVER_URL + "/create_post",
+      {
+        currentUser: user,
+        text: text,
+      })
+  );
+}
 
-    function createPostWithPicture(user, picture, text) {
-        postWithFile(
-            SERVER_URL + "/create_post",
-            picture,
-            "pictureFile",
-            {
-                currentUser: user,
-                text: text
-            });
-        window.location = "index.html";
-    }
+function createPostWithPicture(user, picture, text) {
+  postWithFile(
+    SERVER_URL + "/create_post",
+    picture,
+    "pictureFile",
+    {
+      currentUser: user,
+      text: text
+    });
+  window.location = "index.html";
+}
 
-    function editPost(user, postId, text){
-    	console.log(
-    		$.post(SERVER_URL + "/edit_post/" + postId,
-    		{
-    			currentUser: user,
-    			text: text,
-    		})
-    	);
-    }
+function editPost(user, postId, text) {
+  console.log(
+    $.post(SERVER_URL + "/edit_post/" + postId,
+      {
+        currentUser: user,
+        text: text,
+      })
+  );
+}
 
-    function postWithFile(url, file, fileField, otherFields) {
-        let fd = new FormData();
-        fd.append(fileField, file);
-        let data = [];
-        data.push(otherFields);
-        fd.append("data", JSON.stringify(data));
-        console.log(
-            $.ajax(
-                {
-                  url: url,
-                  type: "POST",
-                  processData: false,
-                  contentType: false,
-                  data: fd
-                }
-            )
-        );
-    }
+function postWithFile(url, file, fileField, otherFields) {
+  let fd = new FormData();
+  fd.append(fileField, file);
+  let data = [];
+  data.push(otherFields);
+  fd.append("data", JSON.stringify(data));
+  console.log(
+    $.ajax(
+      {
+        url: url,
+        type: "POST",
+        processData: false,
+        contentType: false,
+        data: fd
+      }
+    )
+  );
+}
 
-    function editPostWithPicture(user, postId, picture, text) {
-        postWithFile( `${SERVER_URL}/edit_post/${postId}`, picture, "pictureFile",
-            {
-                currentUser: user,
-                text: text
-            });
-    }
+function editPostWithPicture(user, postId, picture, text) {
+  postWithFile(`${SERVER_URL}/edit_post/${postId}`, picture, "pictureFile",
+    {
+      currentUser: user,
+      text: text
+    });
+}
 
-    function deletePost(postId){
-        console.log(postId);
-        $.post(SERVER_URL + "/delete_post/" + postId);
-        window.location = "index.html";
-    }
+function deletePost(postId) {
+  console.log(postId);
+  $.post(SERVER_URL + "/delete_post/" + postId);
+  window.location = "index.html";
+}
 
 //for index.html
-    function viewFeed(user){
+function viewFeed(user) {
 
-    	//user = testUser;
-
-
-    		$.post(SERVER_URL + "/feed",
-    		{
-    			currentUser: user,
-    		}, function(data){
-                console.log(data);
-                let html_to_append = '';
-
-                $.each(data, function(i, item)
-                {
-
-html_to_append +=   '<div class="col-xs-12 offset-xl-1 col-xl-10 column-3">' +
-                      '<div class="subgrid">' +
-                        '<div class="row subgrid-row-2">' +
-                          '<div class="col-xs-3 offset-xs-1 col-md-2">' +
-                            '<div class="responsive-picture picture-2">' +
-                              '<picture><img alt="Placeholder Picture" src="img/picture.svg">' +
-                              '</picture>' +
-                            '</div>' +
-                          '</div>' +
-                          '<div class="col-xs-3">' +
-                            '<a class="link-text text-link-1 profileUsername">' + item.username + '</a>' +
-                          '</div>' +
-                          '<div class="col-xs-2"><a class="link-button btn viewbtn" title="">Follow</a>' +
-                          '</div>' +
-                        '</div>' +
-                      '</div>' +
-                      '<div class="subgrid">' +
-                        '<div class="row subgrid-row-1">' +
-                          '<div class="col-xs-10 push-xs-0 offset-xs-1">' +
-                            '<div class="responsive-picture picture-1">' +
-                              '<picture><img alt="Placeholder Picture" src="' + IMAGE_HOST_URL + item.picture + '">' +
-                              '</picture>' +
-                            '</div>' +
-                          '</div>' +
-                        '</div>' +
-                        '<div class="row subgrid-row-2">' +
-                          '<div class="col-xs-12 col-xl-12">' +
-                            '<p class="paragraph paragraph-2">' + item.text + '</p>' +
-                          '</div>' +
-                          '<div class="col-xs-8 push-xs-2"><a class="link-button btn viewbtn deletePostButton" value="' + item.id + '" title="">Delete Post</a><a class="link-button btn viewbtn viewPostButton" name="' + item.username +  '" value="' + item.id + '" title="">View Post</a>' +
-                          '</div>' +
-                        '</div>' +
-                      '</div>' +
-                    '</div>'
-                });
-                $(".postRow").html(html_to_append);
-                //$('.profileUsername').click(function() {
-                //    alert($(this).text());
-                //});
-                $('.profileUsername').click(function() {
-                    window.location = "profilepage.html#" + $(this).text();
-                });
-
-                $('.viewPostButton').click(function() {
-                    console.log($(this)[0].getAttribute("value"));
-                    window.location = "viewpost.html#" + $(this)[0].getAttribute("name") + '&' + $(this)[0].getAttribute("value") ;
-                });
-
-                $('.deletePostButton').click(function() {
-                    console.log($(this)[0].getAttribute("value"));
-                    deletePost($(this)[0].getAttribute("value"));
-                });
-
-            });
+  //user = testUser;
 
 
-    }
+  $.post(SERVER_URL + "/feed",
+    {
+      currentUser: user,
+    }, function (data) {
+      console.log(data);
+      let html_to_append = '';
 
-    function redirect(){
-            $('.profileUsername').click(function() {
-            alert($(this).text());
-        });
-    }
+      $.each(data, function (i, item) {
 
-    function search(query){
-    	console.log(
-    		$.post(SERVER_URL + "/search/" + query,
-    		{
-    		})
-    	);
-    }
+        html_to_append += '<div class="col-xs-12 offset-xl-1 col-xl-10 column-3">' +
+          '<div class="subgrid">' +
+          '<div class="row subgrid-row-2">' +
+          '<div class="col-xs-3 offset-xs-1 col-md-2">' +
+          '<div class="responsive-picture picture-2">' +
+          '<picture><img alt="Placeholder Picture" src="img/picture.svg">' +
+          '</picture>' +
+          '</div>' +
+          '</div>' +
+          '<div class="col-xs-3">' +
+          '<a class="link-text text-link-1 profileUsername">' + item.username + '</a>' +
+          '</div>' +
+          '<div class="col-xs-2"><a class="link-button btn viewbtn" title="">Follow</a>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+          '<div class="subgrid">' +
+          '<div class="row subgrid-row-1">' +
+          '<div class="col-xs-10 push-xs-0 offset-xs-1">' +
+          '<div class="responsive-picture picture-1">' +
+          '<picture><img alt="Placeholder Picture" src="' + IMAGE_HOST_URL + item.picture + '">' +
+          '</picture>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+          '<div class="row subgrid-row-2">' +
+          '<div class="col-xs-12 col-xl-12">' +
+          '<p class="paragraph paragraph-2">' + item.text + '</p>' +
+          '</div>' +
+          '<div class="col-xs-8 push-xs-2"><a class="link-button btn viewbtn deletePostButton" value="' + item.id + '" title="">Delete Post</a><a class="link-button btn viewbtn viewPostButton" name="' + item.username + '" value="' + item.id + '" title="">View Post</a>' +
+          '</div>' +
+          '</div>' +
+          '</div>' +
+          '</div>'
+      });
+      $(".postRow").html(html_to_append);
+      //$('.profileUsername').click(function() {
+      //    alert($(this).text());
+      //});
+      $('.profileUsername').click(function () {
+        window.location = "profilepage.html#" + $(this).text();
+      });
 
-    function follow(user, userToFollow){
+      $('.viewPostButton').click(function () {
+        console.log($(this)[0].getAttribute("value"));
+        window.location = "viewpost.html#" + $(this)[0].getAttribute("name") + '&' + $(this)[0].getAttribute("value");
+      });
 
-    	console.log(
-    		$.post(SERVER_URL + "/follow/" + userToFollow,
-    		{
-    			currentUser: user,
-    		})
-    	);
-    }
+      $('.deletePostButton').click(function () {
+        console.log($(this)[0].getAttribute("value"));
+        deletePost($(this)[0].getAttribute("value"));
+      });
+
+    });
+
+
+}
+
+function redirect() {
+  $('.profileUsername').click(function () {
+    alert($(this).text());
+  });
+}
+
+function search(query) {
+  console.log(
+    $.post(SERVER_URL + "/search/" + query,
+      {})
+  );
+}
+
+function follow(user, userToFollow) {
+
+  console.log(
+    $.post(SERVER_URL + "/follow/" + userToFollow,
+      {
+        currentUser: user,
+      })
+  );
+}
 
 //for followButton
-    $( document ).ready(function(e) {
-        $(".followButton").click(function(){
-            follow(document.getElementById)
-        });
-    });    
+$(document).ready(function (e) {
+  $(".followButton").click(function () {
+    follow(document.getElementById)
+  });
+});
 
-    function following(user){
+function following(user) {
 
-    	//user = testUser;
-    	console.log(
-    		$.post(SERVER_URL + "/following/" + user,
-    		{
-    			currentUser: user,
-    		})
-    	);
-    }      
-
-
+  //user = testUser;
+  console.log(
+    $.post(SERVER_URL + "/following/" + user,
+      {
+        currentUser: user,
+      })
+  );
+}
 
 
 /* index.html
@@ -300,7 +297,7 @@ html_to_append +=   '<div class="col-xs-12 offset-xl-1 col-xl-10 column-3">' +
   <script src="js/routeFunctions.js"></script>
   <script> viewFeed("db_test2") </script>
   <script> getCurrentUser() </script>
-*/ 
+*/
 
 /* signup.html
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -374,8 +371,6 @@ html_to_append +=   '<div class="col-xs-12 offset-xl-1 col-xl-10 column-3">' +
   viewUserProfile(userProfile); 
   </script>
 */
-
-
 
 
 /* signup.html do not use, still trying to figure out how to upload pictures
